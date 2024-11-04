9cd947d3f4180fc79633c0ef9c6deb00a576f626
Minato_1737 <sidhdeshvadgaonkar@outlook.com>
2024-11-05 01:02:36 +1100

    fixed issues

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,11 +6,11 @@ plugins {
 
 android {
     namespace = "com.example.clock"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.clock"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
