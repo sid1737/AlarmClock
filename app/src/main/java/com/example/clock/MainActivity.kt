9f2e3ffe239ba2e9e6a0dfbae25f29d4efe89f77
Minato_1737 <sidhdeshvadgaonkar@outlook.com>
2024-11-07 23:12:17 +1100

    - renamed Screen folder

--- app/src/main/java/com/example/clock/MainActivity.kt
+++ app/src/main/java/com/example/clock/MainActivity.kt
@@ -10,7 +10,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.clock.ui.alarmList.components.AlarmCard
+import com.example.clock.ui.alarmListScreen.components.AlarmCard
 import com.example.clock.ui.theme.ClockTheme
 
 class MainActivity : ComponentActivity() {
--- app/src/main/java/com/example/clock/ui/alarmListScreen/components/AlarmCard.kt
+++ app/src/main/java/com/example/clock/ui/alarmListScreen/components/AlarmCard.kt
@@ -1,4 +1,4 @@
-package com.example.clock.ui.alarmList.components
+package com.example.clock.ui.alarmListScreen.components
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
