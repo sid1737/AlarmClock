9a836508140a2415c253b8eca3e58d7128766663
Minato_1737 <sidhdeshvadgaonkar@outlook.com>
2024-11-08 22:59:33 +1100

    - Added Splash Screen

--- app/src/main/java/com/example/clock/MainActivity.kt
+++ app/src/main/java/com/example/clock/MainActivity.kt
@@ -9,13 +9,15 @@ import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.clock.ui.navigation.MainNavController
 import com.example.clock.ui.theme.ClockTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             ClockTheme {
                 Surface(
